e89023d917750dd23d2a87888382eb9f250fd29d
David <david.ndiba@student.moringaschool.com>
2023-06-11 12:14:23 +0300

    Done with Assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,12 @@
 require ( './root.js' );
-
+const name = "susan";
+const height = 40;
 const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(susan).toEqual("Susan");
     });
   });
 
